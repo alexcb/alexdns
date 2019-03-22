20515ce2545088ccb046d79fa4609f0bc3f1f3be
Alex Couture-Beil <alex@mofo.ca>
2019-03-22 14:05:34 -0700

    fix docker lookup

--- alexdns/handlers/docker.py
+++ alexdns/handlers/docker.py
@@ -6,22 +6,36 @@ from dnslib import * #TODO
 import alexdns.common.acblogger as log
 from alexdns.common.chomp_dot import chomp_dot #TODO i dont like this import
 
-def get_docker_ip(container_name):
-    s = subprocess.check_output(['/usr/bin/docker', 'inspect', container_name])
-    x = json.loads(s)
-    return x[0]['NetworkSettings']['Networks'].values()[0]['IPAddress']
+def split_docker_name(x):
+    x = x.split('.')
+    if len(x) == 1:
+        return x[0], None
+    if len(x) == 2:
+        return x[0], x[1]
+    raise ValueError(x)
 
+def get_docker_ip(container_name):
+    container_name, container_network = split_docker_name(container_name)
+    s = subprocess.check_output(['/usr/bin/docker', 'inspect', container_name], encoding='ascii')
+    x = json.loads(s)
+    for network_name, vals in x[0]['NetworkSettings']['Networks'].items():
+        if network_name == container_network or container_network is None:
+            return vals['IPAddress']
+    raise RuntimeError('failed to get docker ip')
 
 def docker(d):
     qname = chomp_dot(str(d.q.qname))
 
     # optimization to avoid shelling out
-    if '.' in qname:
+    if qname.count('.') > 1:
+        return False, d
+    if qname.count('.') == 1 and qname.endswith('.com'):
         return False, d
 
     try:
         ip = get_docker_ip(qname)
-    except:
+    except Exception as e:
+        log.error("failed to get docker ip", e=e, qname=qname)
         return False, d
 
     log.info("resolved docker hostname", host=d, override=ip)
